d85c78c394ada3a70fcaaaa22443a283b8c5f505
Feng Guo <fguo81@gmail.com>
2018-09-11 11:13:59 -0500

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Feng Guo on 9/11/18.
+//  Copyright © 2018 Feng Guo. All rights reserved.
+//
+
+import Foundation
+
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
